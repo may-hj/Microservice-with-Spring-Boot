2d98b63a7076775f749abd44a822076e5d6f9b35
may-hj <hyeonjeong9943@gmail.com>
2021-03-22 15:52:00 +0900

    Dummy Data

--- backend/demo/src/main/java/com/example/demo/cmm/controller/HomeController.java
+++ backend/demo/src/main/java/com/example/demo/cmm/controller/HomeController.java
@@ -1,8 +1,18 @@
 package com.example.demo.cmm.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		System.out.println("===============================");
+		System.out.println("======== Home Connected =======");
+		System.out.println("===============================");
+		
+		return "<h1>Hello Wolrd!!<h1>";
+	}
 
 }
